8097a82ff6df8df8abb9ff516014c00918771018
Jeremy Chone <jeremy.chone@gmail.com>
2022-01-03 15:24:29 -0800

    . update to version v0.1.2 (and update dependencies)

--- src/argv.rs
+++ src/argv.rs
@@ -1,24 +1,29 @@
 use clap::{crate_version, App, Arg};
 
-pub fn cmd_app() -> App<'static, 'static> {
+pub fn cmd_app() -> App<'static> {
 	let app = App::new("naudit")
 		.version(&crate_version!()[..])
 		.about("npm multi package audit")
 		.arg(
-			Arg::with_name("clean")
-				.short("c")
+			Arg::new("clean")
+				.short('c')
 				.long("clean")
 				.help("clean the node_modules and package-lock.json")
 				.takes_value(false),
 		)
 		.arg(
-			Arg::with_name("no_install")
+			Arg::new("no_install")
 				.long("no-install")
 				.help("Do not do a npm install")
 				.takes_value(false),
 		)
-		.arg(Arg::with_name("no_audit").long("no-audit").help("Do not do a npm audit").takes_value(false))
-		.arg(Arg::with_name("PATH").help("Path to root"));
+		.arg(
+			Arg::new("no_audit")
+				.long("no-audit")
+				.help("Do not do a npm audit")
+				.takes_value(false),
+		)
+		.arg(Arg::new("PATH").help("Path to root"));
 
 	app
 }
